96e5ee79ba3a838810bdc260dcc0c9f2a8f66c26
brylie_baird <bryliebaird@gmail.com>
2021-11-24 13:39:30 -0600

    fetch all repos and displaying them on page.

--- js/script.js
+++ js/script.js
@@ -1,11 +1,14 @@
 const overviewDiv = document.querySelector(".overview");
+const repoList = document.querySelector(".repo-list");
 const username = "bryliebaird";
+const repo = "guess-the-word";
+
 
 //Fetch API JSON Data from Github API 
 const getUserData = async function(){
     const res = await fetch (`https://api.github.com/users/${username}`);
     const userData = await res.json();
-    console.log(userData);
+    //console.log(userData);
   
     // Fire displayUserData function
     displayUserData(userData);
@@ -16,18 +19,18 @@ getUserData();
 
 
 const displayUserData = function(userData){
-    console.log(`displayUserData function is running and can pass userData ${userData}`);
+    //console.log(`displayUserData function is running and can pass userData ${userData}`);
 
     let userInformation = document.createElement("div");
     userInformation.classList.add("user-info");
 
-     const avatar = userData.avatar_url;
-     const name = userData.name;
-     const bio = userData.bio;
-     const location = userData.location;
-     const repoNum = userData.public_repos;
+    const avatar = userData.avatar_url;
+    const name = userData.name;
+    const bio = userData.bio;
+    const location = userData.location;
+    const repoNum = userData.public_repos;
 
-     console.log(name);
+    //console.log(name);
 
     userInformation.innerHTML = 
     `  <figure>
@@ -41,4 +44,26 @@ const displayUserData = function(userData){
         </div> `
 
     overviewDiv.append(userInformation);
+}
+
+
+const getRepos = async function(){
+    const res = await fetch(`https://api.github.com/users/${username}/repos?sort=updated&per_page=100`)
+    const repoData = await res.json();
+    console.log(repoData);
+    displayRepo(repoData);
+}
+
+getRepos();
+
+const displayRepo = function(repoData){
+    for(const item of repoData){
+        let li = document.createElement("li");
+        let title = document.createElement("h3");
+        title.innerText = item.name;
+        li.append(title);
+        li.classList.add("repo");
+        repoList.append(li);
+        //console.log(repoList, li);
+    }
 }
